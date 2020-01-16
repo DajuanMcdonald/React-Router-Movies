9a7034c5b204ef7096eb28a8dbb26306ebbd0dcc
fiopwk <flopwiki@gmail.com>
2020-01-15 19:30:43 -0500

    added functional Home button to SavedList component

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import {Link, NavLink} from "react-router-dom";
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
+      <span className="saved-movie">{movie.title}
+      <NavLink activeStyle={{fontWeight: 'italic', color: 'crimson',}}
+               to={`/movies/${movie.id}`}>
+
+      </NavLink>
+
+      </span>
     ))}
     <div className="home-button">Home</div>
+      <Link to="/">Home</Link>
   </div>
 );
 
